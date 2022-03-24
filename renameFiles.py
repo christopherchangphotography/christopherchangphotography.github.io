afda9a924005965610549596ecedc68ff16ef3e7
Aaron <56976471+aaronwangj@users.noreply.github.com>
2022-03-24 13:35:15 -0400

    remove tags

---
+++ renameFiles.py
@@ -0,0 +1,24 @@
+
+# Pythono3 code to rename multiple
+import os
+# files in a directory or folder
+ 
+# importing os module
+def main():
+    print(os.getcwd())
+
+    for filename in os.listdir("images/fulls"):
+        x = filename.replace(' ', '').replace("Medium", "").replace("Large","")
+        print(x)
+        os.rename("images/fulls/" + filename, "images/fulls/" + x)
+        
+    for filename in os.listdir("images/thumbs"):
+        x = filename.replace(' ', '').replace("Medium", "").replace("Large","")
+        print(x)
+        os.rename("images/thumbs/" + filename,"images/thumbs/" + x)
+
+# Driver Code
+if __name__ == '__main__':
+     
+    # Calling main() function
+    main()
